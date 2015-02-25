8812b9682115c6dd9c984488539314bab06b5229
Chris Schipper <schippcj@miamioh.edu>
2015-02-24 21:47:13 -0500

    Modified the explore page filter

--- app/controllers/explore.js
+++ app/controllers/explore.js
@@ -17,11 +17,11 @@ checked13 = false;
 popIsUp = 0;
 
 // dummy click event.  Add your own!
-function testclick(e){
+function testclick(e) {
 	alert('Clicked ' + '\'' + e.source.id + '\'');
 }
 
-function searchDisplay(){
+function searchDisplay() {
 	$.searchBar.visible = true;
 	$.cancel.visible = true;
 	$.menubtn.visible = false;
@@ -29,8 +29,8 @@ function searchDisplay(){
 	$.logobtn.visible = false;
 }
 
-function cancelSearch(){
-	$.searchBar.value="";
+function cancelSearch() {
+	$.searchBar.value = "";
 	$.searchBar.visible = false;
 	$.cancel.visible = false;
 	$.menubtn.visible = true;
@@ -39,39 +39,38 @@ function cancelSearch(){
 }
 
 function signOut() {
-    var index = Alloy.createController('index').getView();
-    index.open();
+	var index = Alloy.createController('index').getView();
+	index.open();
 }
 
 // handles the "Like button"
-function likeunlike(e){
+function likeunlike(e) {
 	// change like/unlike image, based on the current image
-	if (e.source.image==="/330-Dollar_30.png"){
-		e.source.image="/Green-Dollar-Sign.png"
-	}else{
-		e.source.image="/330-Dollar_30.png"
+	if (e.source.image === "/330-Dollar_30.png") {
+		e.source.image = "/Green-Dollar-Sign.png"
+	} else {
+		e.source.image = "/330-Dollar_30.png"
 	}
 }
 
-
 // shows or hide the menu
 var menuOpen = false;
-function showhidemenu(e){
-	if (!menuOpen){
-		moveTo="60%";
-		menuOpen=true;
-	}else{
-		moveTo="0";
-		menuOpen=false;
+function showhidemenu(e) {
+	if (!menuOpen) {
+		moveTo = "60%";
+		menuOpen = true;
+	} else {
+		moveTo = "0";
+		menuOpen = false;
 	}
-	
+
 	// have to set the current width of the "main" view before moving it so it doesn't get squeezed
-	// try commenting out the following line and setting the "newLeft" to 200 instead of 
+	// try commenting out the following line and setting the "newLeft" to 200 instead of
 	// 300 to see what I mean
-	$.main.width=Ti.UI.FILL;
+	$.main.width = Ti.UI.FILL;
 	$.main.animate({
-		left:moveTo,
-		duration:100
+		left : moveTo,
+		duration : 100
 	});
 }
 
@@ -79,7 +78,7 @@ function showhidemenu(e){
 /**
  * Opens My_Roster Page
  */
-function openMyRoster(){
+function openMyRoster() {
 	var my_Roster = Alloy.createController('my_Roster').getView();
 	my_Roster.open();
 }
@@ -88,42 +87,42 @@ function openMyRoster(){
  * Opens index (home) page
  */
 function openHome() {
-    var index = Alloy.createController('HomePage').getView();
-    index.open();
+	var index = Alloy.createController('HomePage').getView();
+	index.open();
 }
 
 /**
  *Opens Notifications Page
- *  
+ *
  */
 function openNotifications() {
-    var notifications = Alloy.createController('notifications').getView();
-    notifications.open();
+	var notifications = Alloy.createController('notifications').getView();
+	notifications.open();
 }
 
 /**
- *Opens Profile Page 
+ *Opens Profile Page
  */
 function openProfile(e) {
 	Titanium.App.Properties.setString('name', e.source.text);
-Titanium.App.Properties.setString('profilePictureID', e.source.id);
-    var profile = Alloy.createController('profileView').getView();
-    profile.open();
+	Titanium.App.Properties.setString('profilePictureID', e.source.id);
+	var profile = Alloy.createController('profileView').getView();
+	profile.open();
 }
 
-function openExplore(){
+function openExplore() {
 	var explore = Alloy.createController('explore').getView();
 	explore.open();
 }
+
 /////////////////////////End Page Open Statements/////////////////////////
 
-
 /**
- *Operate the Sports Filter 
+ *Operate the Sports Filter
  */
 
-function sportsFilter(){
-	if(popIsUp == 0){
+function sportsFilter() {
+	if (popIsUp == 0) {
 		$.popup.top = '5%';
 		$.popup.left = '15%';
 		$.popup.height = '60%';
@@ -133,8 +132,7 @@ function sportsFilter(){
 		$.popupSpace.height = '100%';
 		$.popupSpace.width = '70%';
 		popIsUp = 1;
-	}
-	else if(popIsUp == 1){
+	} else if (popIsUp == 1) {
 		$.popup.top = '0%';
 		$.popup.left = '0%';
 		$.popup.height = '0%';
@@ -149,174 +147,160 @@ function sportsFilter(){
 
 ////////////////////////////////////////////////@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 /**
- *Operate the Sports Filter 
+ *Operate the Sports Filter
  */
 
-function openFilter(){
-	if(openFilter == 0){
+function openFilter() {
+	if (openFilter == 0) {
 		$.filterList.height = '80%';
 		$.exploreFeed.height = "0%";
 		openFilter = 1;
-		
-	}
-	else if(openFilter == 1){
+
+	} else if (openFilter == 1) {
 		$.filterList.height = '0%';
 		$.exploreFeed.height = "80%";
 		openFilter = 0;
 	}
-	
+
 }
 
-
-
-
-
-
-
-
-
-
- 
 //hard coded checkmark statements. These will need ot be fixed for database usage. Should only keep one when switching over to dB
 function selectCheck1() {
-	if(checked1 == false) {
+	if (checked1 == false) {
 		$.checkMark1.visible = true;
 		checked1 = true;
-	}
-	else {
+	} else {
 		$.checkMark1.visible = false;
 		checked1 = false;
 	}
 }
+
 function selectCheck2() {
-	if(checked2 == false) {
+	if (checked2 == false) {
 		$.checkMark2.visible = true;
 		checked2 = true;
-	}
-	else {
+	} else {
 		$.checkMark2.visible = false;
 		checked2 = false;
 	}
 }
+
 function selectCheck3() {
-	if(checked3 == false) {
+	if (checked3 == false) {
 		$.checkMark3.visible = true;
 		checked3 = true;
-	}
-	else {
+	} else {
 		$.checkMark3.visible = false;
 		checked3 = false;
 	}
 }
+
 function selectCheck4() {
-	if(checked4 == false) {
+	if (checked4 == false) {
 		$.checkMark4.visible = true;
 		checked4 = true;
-	}
-	else {
+	} else {
 		$.checkMark4.visible = false;
 		checked4 = false;
 	}
 }
+
 function selectCheck5() {
-	if(checked5 == false) {
+	if (checked5 == false) {
 		$.checkMark5.visible = true;
 		checked5 = true;
-	}
-	else {
+	} else {
 		$.checkMark5.visible = false;
 		checked5 = false;
 	}
 }
+
 function selectCheck6() {
-	if(checked6 == false) {
+	if (checked6 == false) {
 		$.checkMark6.visible = true;
 		checked6 = true;
-	}
-	else {
+	} else {
 		$.checkMark6.visible = false;
 		checked6 = false;
 	}
 }
+
 function selectCheck7() {
-	if(checked7 == false) {
+	if (checked7 == false) {
 		$.checkMark7.visible = true;
 		checked7 = true;
-	}
-	else {
+	} else {
 		$.checkMark7.visible = false;
 		checked7 = false;
 	}
 }
+
 function selectCheck8() {
-	if(checked8 == false) {
+	if (checked8 == false) {
 		$.checkMark8.visible = true;
 		checked8 = true;
-	}
-	else {
+	} else {
 		$.checkMark8.visible = false;
 		checked8 = false;
 	}
 }
+
 function selectCheck9() {
-	if(checked9 == false) {
+	if (checked9 == false) {
 		$.checkMark9.visible = true;
 		checked9 = true;
-	}
-	else {
+	} else {
 		$.checkMark9.visible = false;
 		checked9 = false;
 	}
 }
+
 function selectCheck10() {
-	if(checked10 == false) {
+	if (checked10 == false) {
 		$.checkMark10.visible = true;
 		checked10 = true;
-	}
-	else {
+	} else {
 		$.checkMark10.visible = false;
 		checked10 = false;
 	}
 }
+
 function selectCheck11() {
-	if(checked11 == false) {
+	if (checked11 == false) {
 		$.checkMark11.visible = true;
 		checked11 = true;
-	}
-	else {
+	} else {
 		$.checkMark11.visible = false;
 		checked11 = false;
 	}
 }
+
 function selectCheck12() {
-	if(checked12 == false) {
+	if (checked12 == false) {
 		$.checkMark12.visible = true;
 		checked12 = true;
-	}
-	else {
+	} else {
 		$.checkMark12.visible = false;
 		checked12 = false;
 	}
 }
+
 function selectCheck13() {
-	if(checked13 == false) {
+	if (checked13 == false) {
 		$.checkMark13.visible = true;
 		checked13 = true;
-	}
-	else {
+	} else {
 		$.checkMark13.visible = false;
 		checked13 = false;
 	}
 }
 
-
 ///////////END HARD CODED METHODS////////////////////////////
 
-
-// This bit listens to the orientation change and re-establishes the width 
+// This bit listens to the orientation change and re-establishes the width
 // of the "main" view, allowing the layout to survive after orientation changes
 Ti.Gesture.addEventListener('orientationchange', function(e) {
-    $.main.width=Ti.Platform.displayCaps.platformWidth;
+	$.main.width = Ti.Platform.displayCaps.platformWidth;
 });
 
-$.explore.open();
+$.explore.open(); 
