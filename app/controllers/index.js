e00aa9be7e4c5054fce50a8f91b8d06ae7fcecee
Chris Schipper <schippcj@miamioh.edu>
2015-03-03 00:50:23 -0500

    Created buttons. Mark actually coded the majority of this commit on my computer

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,15 +1,20 @@
+function LoginClicked(e) {
+	if ($.email.value.toUpperCase() == "DEMO" && $.password.value == "demo") {
+		var HomePage = Alloy.createController('HomePage').getView();
+		HomePage.open();
+		$.index.close();
+	} else {
+		alert("Invalid email/password");
+	}
+};
+function SignUpClicked(e) {
+	$.SignUp_Athlete.visible = true;
+	$.SignUp_Fan.visible = true;
+	$.SignUp.visible = false;
+};
 
-function LoginClicked(e){
-if($.email.value.toUpperCase() == "DEMO" && $.password.value == "demo"){
-var HomePage = Alloy.createController('HomePage').getView();
-HomePage.open();
-$.index.close();
-}
-else{
-alert("Invalid email/password");
-}
-};
-function SignUpClicked(e){
-alert("SignUp clicked.");
-};
-$.index.open({modal: true});
+
+
+$.index.open({
+	modal : true
+}); 
